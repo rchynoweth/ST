e77c953f50805cee35742f23a94b64af60eda565
rchynoweth <79483287+rchynoweth@users.noreply.github.com>
2023-08-01 14:40:17 -0700

    Update NumberOfJobs_Estimated.sql

--- system_table_analytics/queries/NumberOfJobs_Estimated.sql
+++ system_table_analytics/queries/NumberOfJobs_Estimated.sql
@@ -1,7 +1,7 @@
 SELECT 
 DATE_FORMAT(usage_start_time, 'yyyy-MM') as MonthYear,
 
-COUNT(DISTINCT concat(usage_metadata.job_id, usage_metadata.cluster_id)) as WorkspaceCount
+COUNT(DISTINCT concat(usage_metadata.job_id, usage_metadata.cluster_id)) as JobCount
 
 FROM system.billing.usage
 
